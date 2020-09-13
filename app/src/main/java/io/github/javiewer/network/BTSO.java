638cc3bc4da2965f3cb4eb1af2a6897d9ef1fe31
lao <498690132@qq.com>
2020-09-13 22:56:10 +0800

    Revert "Merge branch 'master' of https://github.com/ccclao/JAViewer into fuck"
    This reverts commit 4e576fba9d59299090dce9356a71262d6ec2156d, reversing
    changes made to cb7c4783f18db66f24138e75d7a9c7879212626b.

--- app/src/main/java/io/github/javiewer/network/BTSO.java
+++ app/src/main/java/io/github/javiewer/network/BTSO.java
@@ -6,8 +6,9 @@ import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
-import retrofit2.http.Query;
+import retrofit2.http.Path;
 import retrofit2.http.Url;
+
 
 /**
  * BTSO
@@ -23,16 +24,15 @@ public interface BTSO {
     String BASE_URL = "https://btsow.com";
     String BASE_URL1 = "https://btsow.monster";
     String BASE_URL2 = "https://btsow.case";
-    String BASE_URL3 = "https://api.rekonquer.com";
     BTSO INSTANCE = new Retrofit.Builder()
             .baseUrl(BTSO.BASE_URL)
             .client(JAViewer.HTTP_CLIENT)
             .build()
             .create(BTSO.class);
 
-    @GET("/btso.php")
+    @GET("/search/{keyword}/page/{page}")
     @Headers("Accept-Language: zh-CN,zh;q=0.8,en;q=0.6")
-    Call<ResponseBody> search(@Query(value = "kw") String keyword, @Query("page") int page);
+    Call<ResponseBody> search(@Path(value = "keyword") String keyword, @Path("page") int page);
 
     @GET
     @Headers("Accept-Language: zh-CN,zh;q=0.8,en;q=0.6")
